0d9f26ae3faf8fe41da2be9ea35e85bdbc67696e
josman185 <josman185@hotmail.com>
2024-05-03 11:11:46 -0600

    Initial Commit

---
+++ Scrumdinger/Views/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Jose Vargas on 5/2/24.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
